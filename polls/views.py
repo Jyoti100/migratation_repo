fca3270d90273dd41524ff2255d33a6885554cb7
glo34645 <glo34645@adobe.com>
2022-12-24 12:06:50 +0530

    small change

--- polls/views.py
+++ polls/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponse
 
 
 def index(request):
+    print("version test ")
     return HttpResponse("Hello, world. You're at the polls index.{}".format(request.current_time))
