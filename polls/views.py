6944c31121bca7d0f5dc389569bccd823b5821d0
glo34645 <glo34645@adobe.com>
2022-12-30 10:19:20 +0530

    commitig

--- polls/views.py
+++ polls/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    print("version test ")
+    print("version test hello")
     return HttpResponse("Hello, world. You're at the polls index.{}".format(request.current_time))
